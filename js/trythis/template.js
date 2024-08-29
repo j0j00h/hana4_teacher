babd2da519c42949d763507121cdfb90dbf9ad93
indiflex <indiflex1@gmail.com>
2024-08-29 09:42:03 +0900

    getNextWeek

--- js/trythis/template.js
+++ js/trythis/template.js
@@ -1,7 +1,7 @@
 const { rejects } = require('assert');
 
 const before = () => console.log('before....');
-const after = () => console.log('after...');
+const after = r => console.log('after...', r);
 
 const someFn = (name, greeting) => console.log(`${greeting}, ${name}`);
 const someFn2 = (id, nickname, email, level) =>
@@ -9,15 +9,16 @@ const someFn2 = (id, nickname, email, level) =>
 
 const template =
   f =>
-  async (...args) => {
+  (...args) => {
     before();
     const ret = f(...args);
-    // after();
-    setTimeout(after);
+    after(ret);
+    // setTimeout(() => after(ret));
     return ret;
 
     // return new Promise(resolve => {
     //   resolve(f(...args));
+    //   after();
     // }).finally(after);
   };
 
@@ -27,9 +28,10 @@ const temp2 = template(someFn2); // before → someFn2 → after 실행
 // // temp2(1, 'sico', 'sico@gmail.com', 5);
 // console.log('square of 7 =', await template(n => n ** 2)(7));
 
-const x = template(n => n ** 2);
-(async function ff() {
-  await temp('sico', 'hello');
-  // temp2(1, 'sico', 'sico@gmail.com', 5);
-  console.log('square of 7 =', await x(7));
-})();
+temp('sico', 'hello');
+const x = template(n => n ** 2)(7);
+// (async function ff() {
+//   await temp('sico', 'hello');
+//   // temp2(1, 'sico', 'sico@gmail.com', 5);
+//   console.log('square of 7 =', await x(7));
+// })();
