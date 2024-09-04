14298dacbe6e813a4368fae100f5168f8bb61700
indiflex <indiflex1@gmail.com>
2024-09-04 17:30:29 +0900

    telfmt

--- js/trythis/regexp.js
+++ js/trythis/regexp.js
@@ -61,7 +61,7 @@ const 자음알파벳숫자 = [...'LMNRlmnr136780'].map(a => a.charCodeAt());
 function isEndJaum(str) {
   const s = str.charCodeAt(str.length - 1);
   // console.log('🚀  s:', s, str.charCodeAt(str.length - 1));
-  console.log(str, s - 가, (s - 가) % 28, 자음알파벳숫자.includes(s));
+  // console.log(str, s - 가, (s - 가) % 28, 자음알파벳숫자.includes(s));
   // if (s >= ㄱ && s <= ㅎ) return true;
   // if (자음알파벳숫자.includes(s)) return true;
 
@@ -91,3 +91,26 @@ assert.equal(isEndJaum('점수 A'), false);
 assert.equal(isEndJaum('알파벳L'), true);
 assert.equal(isEndJaum('24'), false);
 assert.equal(isEndJaum('23'), true);
+
+// -------------------
+const telfmt = str => {
+  const len = str.length;
+  if (len < 7) return str;
+  if (len <= 8) return str.replace(/(\d{3,4})(\d{4})$/g, '$1-$2');
+
+  // const g3 = 4;
+  const g1 = str.startsWith('02') ? 2 : len === 12 ? 4 : 3;
+  const g2 = len - g1 - 4;
+  // console.log(str, '->', g1, g2, g3);
+
+  const regexp = new RegExp(`(\\d{${g1}})(\\d{${g2}})(\\d{4})$`, 'g');
+  return str.replace(regexp, '$1-$2-$3');
+};
+assert.deepStrictEqual(telfmt('0101234567'), '010-123-4567');
+assert.deepStrictEqual(telfmt('01012345678'), '010-1234-5678');
+assert.deepStrictEqual(telfmt('0212345678'), '02-1234-5678');
+assert.deepStrictEqual(telfmt('021234567'), '02-123-4567');
+assert.deepStrictEqual(telfmt('0331234567'), '033-123-4567');
+assert.deepStrictEqual(telfmt('15771577'), '1577-1577');
+assert.deepStrictEqual(telfmt('07012341234'), '070-1234-1234');
+assert.deepStrictEqual(telfmt('050712345678'), '0507-1234-5678');
