b45a66277e31058b939aa637516b9241f91e6328
indiflex <indiflex1@gmail.com>
2024-09-06 17:43:57 +0900

    async-await trythis

--- js/trythis/promiseAll.js
+++ js/trythis/promiseAll.js
@@ -29,21 +29,63 @@ const promiseAll = promises =>
     }
   });
 
-console.time('PALL');
-promiseAll([randTime(1), randTime(2), randTime(3)])
-  .then(arr => {
-    console.table(arr);
-    console.timeEnd('PALL');
-    // assert.deepStrictEqual(arr, vals);
-  })
-  .catch(console.error);
+const promiseAllAsync = async promises => {
+  const results = [];
+  let idx = 0;
+  for (const promise of promises) {
+    promise.catch(err => err);
+    // promise.catch(err => {
+    //   console.error('EEEEE>>>', err);
+    // });
+  }
 
-promiseAll([randTime(11), Promise.reject('RRR'), randTime(33)])
-  .then(array => {
-    console.table(array);
-    console.log(JSON.stringify(array, null, '  '));
-    console.log('여긴 과연 호출될까?!');
-  })
-  .catch(error => {
-    console.log('reject!!!!!!>>', error);
-  });
+  for (const promise of promises) {
+    // results[idx++] = await promise;
+    results[idx++] = await promise;
+    // results[promises.indexOf(promise)] = await promise;
+  }
+  // for await (const promise of promises) {
+  //   results[idx++] = promise;
+  // }
+
+  return results;
+};
+
+console.time('PALL');
+const arr = await promiseAllAsync([randTime(1), randTime(2), randTime(3)]);
+console.table(arr);
+console.timeEnd('PALL');
+assert.deepStrictEqual(arr, [1, 2, 3]);
+
+try {
+  const array = await promiseAllAsync([
+    randTime(11),
+    Promise.reject('RRR'),
+    randTime(33),
+  ]);
+
+  console.table(array);
+  console.log(JSON.stringify(array, null, '  '));
+  console.log('여긴 과연 호출될까?!');
+} catch (error) {
+  console.log('reject!!!!!!>>', error);
+}
+
+// original version
+// promiseAll([randTime(1), randTime(2), randTime(3)])
+//   .then(arr => {
+//     console.table(arr);
+//     console.timeEnd('PALL');
+//     // assert.deepStrictEqual(arr, vals);
+//   })
+//   .catch(console.error);
+
+// promiseAll([randTime(11), Promise.reject('RRR'), randTime(33)])
+//   .then(array => {
+//     console.table(array);
+//     console.log(JSON.stringify(array, null, '  '));
+//     console.log('여긴 과연 호출될까?!');
+//   })
+//   .catch(error => {
+//     console.log('reject!!!!!!>>', error);
+//   });
