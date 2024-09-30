7bf8a01e098ef6e59d5cd7c5952843cef2c534fc
indiflex <indiflex1@gmail.com>
2024-09-30 15:14:07 +0900

    search cart item

--- rbvite/src/components/My.tsx
+++ rbvite/src/components/My.tsx
@@ -2,11 +2,12 @@ import { FaPlus } from 'react-icons/fa6';
 import Login from './Login.tsx';
 import Profile from './Profile.tsx';
 import Button from './atoms/Button.tsx';
-import { useEffect, useLayoutEffect, useMemo, useRef } from 'react';
+import { useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import { useSession } from '../hooks/session-context.tsx';
 import Item from './Item.tsx';
 import useToggle from '../hooks/toggle.ts';
-import { useTimeout } from '../hooks/timer-hooks.ts';
+import { useDebounce, useTimeout } from '../hooks/timer-hooks.ts';
+import { FaSearch } from 'react-icons/fa';
 
 export default function My() {
   const { session, toggleReloadSession } = useSession();
@@ -17,6 +18,19 @@ export default function My() {
   //   setIsAdding((pre) => !pre);
   // };
   const [isAdding, toggleAdding] = useToggle();
+
+  const [, toggleSearch] = useToggle();
+  const searchRef = useRef<HTMLInputElement>(null);
+  const [searchstr, setSearchstr] = useState('');
+
+  useDebounce(
+    () => {
+      console.log('useDebounce.search>>', searchRef.current?.value);
+      setSearchstr(searchRef.current?.value || '');
+    },
+    2000,
+    [searchRef.current?.value]
+  );
 
   // const primitive = 123;
 
@@ -83,29 +97,45 @@ export default function My() {
         <Login />
       )}
 
-      <ul className='mt-3 w-2/3 border p-3'>
-        {session.cart?.length ? (
-          session.cart.map((item) => (
-            <li key={item.id}>
-              <Item item={item} />
-            </li>
-          ))
-        ) : (
-          <li className='text-slate-500'>There is no items.</li>
-        )}
-        <li className='mt-3 text-center'>
-          {isAdding ? (
-            <Item
-              item={{ id: 0, name: '', price: 0 }}
-              toggleAdding={() => toggleAdding()}
-            />
+      <div className='w-2/3 border p-3'>
+        <div className='flex items-center gap-2'>
+          <FaSearch />
+          <input
+            // onChange={(e) => setSearchstr(e.currentTarget.value)}
+            onChange={toggleSearch}
+            ref={searchRef}
+            type='text'
+            placeholder='아이템명 검색...'
+            className='inp'
+          />
+        </div>
+        <ul className='mt-3 px-3'>
+          {session.cart?.length ? (
+            session.cart
+              .filter(({ name }) => name.includes(searchstr))
+              .map((item) => (
+                <li key={item.id}>
+                  <Item item={item} />
+                </li>
+              ))
           ) : (
-            <Button onClick={toggleAdding}>
-              <FaPlus /> Add Item
-            </Button>
+            <li className='text-slate-500'>There is no items.</li>
           )}
-        </li>
-      </ul>
+          <li className='mt-3 text-center'>
+            {isAdding ? (
+              <Item
+                item={{ id: 0, name: '', price: 0 }}
+                toggleAdding={() => toggleAdding()}
+              />
+            ) : (
+              <Button onClick={toggleAdding}>
+                <FaPlus /> Add Item
+              </Button>
+            )}
+          </li>
+        </ul>
+      </div>
+
       <div className='mb-3 flex gap-5'>
         <span>*총액: {totalPrice.toLocaleString()}원</span>
         <span>*할인: {dcPrice.toFixed(0).toLocaleString()}원</span>
