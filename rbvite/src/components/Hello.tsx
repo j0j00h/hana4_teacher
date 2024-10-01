3571fba6a9a712c8e5af27acda9f5783c683f442
indiflex <indiflex1@gmail.com>
2024-10-01 14:44:45 +0900

    make useState by useReducer

--- rbvite/src/components/Hello.tsx
+++ rbvite/src/components/Hello.tsx
@@ -3,7 +3,7 @@ import {
   forwardRef,
   ReactNode,
   useImperativeHandle,
-  useState,
+  useReducer,
 } from 'react';
 import { useCounter } from '../hooks/counter-hook';
 import { useSession } from '../hooks/session-context';
@@ -63,13 +63,49 @@ type PlaceUser = {
   email: string;
 };
 
+// why S & Function is not callable...
+// type X = { id: number } & number;
+// const x: X = { id: 1 };
+// console.log('🚀  x:', x);
+
+/**
+  const [state, setState] = useMyState(...);
+  useMyState(1) | useSuseMyStatetate(() => 1)
+
+  setState(x) 
+    ->-> dispatch(x) =>=> reducer(s, x);
+  setState(pre => ..)
+    ->-> dispatch(pre => ...) =>=> reducer(s, pre => ...);
+ */
+// type WithoutParamFunction<T> = () => T;
+// type SS<X> = (x: X) => X;
+// useMyState(() => () => 1) ,  setState(() => 9)
+function useMyState<S>(init: S | (() => S)) {
+  const [state, dispatch] = useReducer(
+    (pre: S, action: S | ((s: S) => S)) =>
+      isActionFunction<S>(action) ? action(pre) : action,
+    isInitializerFunction<S>(init) ? init() : init
+    // init instanceof Function ? init() : init
+  );
+
+  return [state, dispatch] as const;
+}
+
+function isInitializerFunction<T>(x: unknown): x is () => T {
+  return typeof x === 'function';
+}
+function isActionFunction<T>(x: unknown): x is (t: T) => T {
+  return typeof x === 'function';
+}
+
 function Hello({ friend }: Props, ref: ForwardedRef<MyHandler>) {
   // const [myState, setMyState] = useState(() => new Date().getTime());
   const {
     session: { loginUser },
   } = useSession();
   const { count, plusCount, minusCount } = useCounter();
-  const [myState, setMyState] = useState(0);
+  // const [myState, setMyState] = useState(0);
+  const [myState, setMyState] = useMyState(0);
   let v = 1;
 
   const handler: MyHandler = {
